fa15a8007b477f1404469d47513e96e506bd7c94
Pradeep Ponnusamy <ponnpradeep@gmail.com>
2022-01-03 06:03:23 +0000

    Project Submission

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="prayon/project4"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login &&\
+	    docker image tag project4 $dockerpath
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
